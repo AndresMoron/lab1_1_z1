42f1c6fc7dc128303ed02a08bea43258a5e01edf
Andres Moron <andresmoron622@gmail.com>
2019-03-02 09:56:11 -0400

    Segundo Commit

---
+++ solAritmetica/WcfAritmetica/WSatirmetica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfAritmetica
+{
+    
+    [ServiceContract]
+    public interface WSatirmetica
+    {
+        [OperationContract]
+        clsAritmetica sumar(int numero1, int numbero2);
+
+    }
+    [DataContract]
+    public class clsAritmetica
+    {
+        [DataMember]
+        public int NumeroA { get; set; }
+        [DataMember]
+
+        public int NumeroB { get; set; }
+        [DataMember]
+
+        public int Respuesta { get; set; }
+    }
